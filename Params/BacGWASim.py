7d2be07484ae0e70e13c1ec9122e18e9a86f5d5a
Syzarta <morteza.mahmoudisaber@gmail.com>
2018-11-08 18:03:01 -0500

    Add files via upload

---
+++ Params/BacGWASim.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 17:55:45 2018
+
+@author: Morteza.M Saber
+
+"""
+
+def BacGWASim(Setting,CoreNumber=1,DirectedAcyclicGraph=True,Rulegraph=False,quiet=False):
+    from os import system
+    import subprocess
+    command='snakemake --snakefile %s --cores %s --latency-wait 120 --rerun-incomplete'%(Setting,CoreNumber)
+    if DirectedAcyclicGraph:
+        command+=' --dag | dot -Tpng > %s.png'%(Setting+'_DAG')
+    if Rulegraph:
+        command+=' --rulegraph| dot -Tpng > %s.png'%(Setting+'_RuleGraph')
+    if quiet:
+        command+=' --quiet'
+    SettingPhylogeny=Setting+'Phylogeny'
+    CommandPhylogeny='snakemake --snakefile %s --cores %s '%(SettingPhylogeny,CoreNumber)
+    unlocker='snakemake --snakefile %s --unlock'%Setting
+    import timeit
+    start = timeit.default_timer()
+    subprocess.call(CommandPhylogeny,shell=True)
+    subprocess.call(unlocker,shell=True)
+    subprocess.call(command,shell=True)
+    stop = timeit.default_timer()
+    total_time = stop - start
+    mins, secs = divmod(total_time, 60)
+    hours, mins = divmod(mins, 60)
+    print("Simulation Completed in: %d:%d:%d.\n" % (hours, mins, secs))
+
+    
+    
+from optparse import OptionParser
+import os,sys
+parser = OptionParser()
+parser.add_option('-S','--Setting', dest='Setting',help='Absolute path to the setting file [required]',type='str',default='')
+parser.add_option('-J','--CoreNumber', dest='CoreNumber',help= 'Use at most N cores in parallel (default: 1)',type='int',default=1)
+parser.add_option('-D',action="store_true", dest='DirectedAcyclicGraph',help= 'Print the directed acyclic\
+                        graph of jobs. (Default:False)',default=False)
+parser.add_option('-R',action="store_true", dest='Rulegraph',help="Print the dependency graph of rules. *Caution*Rulegraph can not be set to True if DirectedAcyclicGraph is set to True (default: False)",default=False)
+parser.add_option('-Q',action="store_true", dest='quiet',help= 'Do not output any progress or rule information (default: False)',default=False)
+
+(options,args)=parser.parse_args()
+Setting=options.Setting
+CoreNumber=options.CoreNumber
+DirectedAcyclicGraph=options.DirectedAcyclicGraph
+Rulegraph=options.Rulegraph
+quiet=options.quiet
+
+if DirectedAcyclicGraph==True  and Rulegraph==True:
+	print ("\nSimulatorPipe For Simulating Bacterail Whole Genomes \nOnly one of 'DirectedAcyclicGraph' and 'Rulegraph' can be used\n")
+	parser.print_help()
+	sys.exit(1)
+if (Setting=='') :
+	print ('\nSimulatorPipe For Simulating Bacterail Whole Genomes \nRequired filed(s) not supplied\n')
+	parser.print_help()
+	sys.exit(1)
+import ast
+input_option_dict=ast.literal_eval(options.__str__())
+print ('Entered arguments are...')
+for one in input_option_dict:
+	print (one,input_option_dict[one])
+print ('')
+
+SimulatorPipe(Setting,CoreNumber,DirectedAcyclicGraph,Rulegraph,quiet)
+
