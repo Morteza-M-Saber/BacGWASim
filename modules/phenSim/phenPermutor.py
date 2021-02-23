e66eb90eb92338412c4d6fc136872b72bed43b6c
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-02-23 14:51:09 -0500

    phenSim as script

--- modules/phenSim/phenPermutor.py
+++ modules/phenSim/phenPermutor.py
@@ -1,46 +1,26 @@
+from math import log
+import random
+import os
 
-def get_options():
-    import argparse
+# Snakemake variables
+input_file = snakemake.input.causalPool_bim
+output_file = snakemake.output.causal
+iterationsPhen = snakemake.config["causal_variant_Num"]
+effectSize = snakemake.config["effect_size_ODR"]
 
-    description = 'Recieves list of all variants in plink.bim format and generates a list of randomly \
-    chosen causal variants and OR values to be used by GCTA phenotype simulator'
-    parser = argparse.ArgumentParser(description=description)
-
-    parser.add_argument('--iterationsPhen',
-                        help='Number of causal variants ')
-    parser.add_argument('--replication',
-                        help='Index of replicatin for phenotype simulation ')
-    parser.add_argument('--PlinkFormatCausalBim',
-                        help='List of all variants in plink.bim format from which causal variants are randomly chosen')
-    parser.add_argument('--effectSize',
-                        help='comma-separated OR values to be assigned to causal variants ')
-    parser.add_argument('--out',
-                        help='Complete path to output directory ')
-
-    return parser.parse_args()
-
-options = get_options()
-   
-import random, os
-
-def phenPermutor(iterationsPhen,replication,PlinkFormatCausalBim,effectSize,out):
-        effect_size_ODR=[int(item) for item in effectSize.split(',')]
-        snpList=[]
-        with open(PlinkFormatCausalBim,'r') as file:
-            line=file.readline()
-            while line:
-                if "*" not in line.split()[1]:
-                    snpList.append(line.split()[1])
-                line=file.readline()
-        variantNums= int(iterationsPhen)
-        reps=replication
-        causalNow=random.sample(snpList,variantNums)
-        effectSizeNow=effect_size_ODR*int(variantNums/len(effect_size_ODR))
-        causalVar=[item for item in zip(causalNow,effectSizeNow)]
-        txt=open(out,'w')
-        from math import log
-        for variants in causalVar:
-            txt.write('%s\t%s\n'%(variants[0],log(variants[1])))
-        txt.close()
+snpList = []
+variantNums = int(iterationsPhen)
+effect_size_ODR = [int(item) for item in effectSize.split(',')]
+with open(input_file, 'r') as file:
+    line = file.readline()
+    while line:
+        if "*" not in line.split()[1]:
+            snpList.append(line.split()[1])
+        line = file.readline()
         
-phenPermutor(options.iterationsPhen,options.replication,options.PlinkFormatCausalBim,options.effectSize,options.out)
+causalNow = random.sample(snpList, variantNums)
+effectSizeNow = effect_size_ODR*int(variantNums/len(effect_size_ODR))
+causalVar = [item for item in zip(causalNow, effectSizeNow)]
+with open(output_file, 'w') as txt:
+    for variants in causalVar:
+        txt.write('%s\t%s\n' % (variants[0], log(variants[1])))
--- modules/phenSim/phenPermutor.smk
+++ modules/phenSim/phenPermutor.smk
@@ -1,35 +1,8 @@
-#writing the snakemake file to call variants from simulation
-# -*- coding: utf-8 -*-
-"""
-?? ?? ?????? ????? ?????, ???? ??? ?? ????? ???
-
-"""
-
-import os
-   
-def indexerRep(wildcards):
-    return(int(wildcards.replication_index))
-
-
 
 rule phenPermutor:
     input:
-        causalPool_bim="{output_dir}/simulations/genSim/causalPool.bim",
+        causalPool_bim = "{output_dir}/simulations/genSim/causalPool.bim",
     output:
-        causal=temp("{output_dir}/simulations/phenSim/{replication_index}/causalLoci.snplist"),
-    params:
-        iterationsPhen= config['causal_variant_Num'],
-        shellCallFile=os.path.join(config["outputDIR"],'BacGWASim.log'),
-        phenpermutor=os.path.join('modules','phenSim','phenPermutor.py'),
-        indexrep=indexerRep,
-        effectSize=config['effect_size_ODR'],
-        logNAME="Generate  causalvariant with OR values for phenotype simulation." + strftime("%Y-%m-%d.%H-%M-%S", localtime()),
-    run:
-        causals=input.causalPool_bim
-        out=os.path.join('/'.join(str(output.causal).split('/')[:-2]),str(params.indexrep),'causalLoci.snplist')
-        callString='python3 %s --iterationsPhen %s --replication %s --PlinkFormatCausalBim %s \
-        --effectSize %s --out %s ' %(params.phenpermutor,params.iterationsPhen, \
-                                     params.indexrep,input.causalPool_bim,params.effectSize,out)
-        call('echo "' + str(params.logNAME) + ':\n ' + callString + '\n" >> ' + params.shellCallFile, shell=True)
-        call(callString, shell=True)
-        
+        causal = temp("{output_dir}/simulations/phenSim/{replication_index}/causalLoci.snplist"),
+    script:
+        "phenPermutor.py"
