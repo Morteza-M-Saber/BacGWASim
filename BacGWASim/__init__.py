88109040673960e70497f6de66cd4447a9d4807f
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-03-08 13:54:29 -0500

    Update version and info

--- BacGWASim/__init__.py
+++ BacGWASim/__init__.py
@@ -1,2 +1,2 @@
 _program = "BacGWASim"
-__version__ = "0.1.0"
+__version__ = "2.0.0"
--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     version=__version__,
     author="Masih M. Saber",
     author_email="morteza.mahmoudisaber@gmail.com",
-    description="BacGWASim command line interface",
+    description="BacGWASim: A simulator for Bacterial Machine learning and Genome-wide Association studies.",
+    url="https://github.com/Morteza-M-Saber/BacGWASim"
     license="MIT",
     packages=["BacGWASim"],
     entry_points="""
@@ -15,4 +16,5 @@ setup(
     {program} = BacGWASim.main:main
     """.format(program=_program),
     include_package_data=True,
+    
 )
